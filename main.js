ee45e902b6c04fac2c3a9a0cb4c971e553a7b1c4
Anton Nemesh <anton.nemesh@gmail.com>
2020-09-09 13:01:14 +0300

    finished task

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 $(document).ready ( function(){
+
     var inputField = $('input');
     var outputField = $('h2');
+    var regex = new RegExp('[^a-zA-Z0-9]+');
+
     $('button').on('click', function() {
+
         outputField.text(inputField.val());
-        if (inputField.val().length > 3) {
-            outputField.addClass('green')
-        } else {
-            outputField.removeClass('green')
-        }
+
+        if (inputField.val().length > 3) { outputField.addClass('green') }
+        else { outputField.removeClass('green') }
+        if (regex.test(inputField.val())) { outputField.addClass('red') }
+        else { outputField.removeClass('red') }
+
         inputField.val('');
+
     });
+
 });
